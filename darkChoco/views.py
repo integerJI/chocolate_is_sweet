3f10e5830bf302def8b8991a0855b34a1bef40a0
integerJI <jjs9536@gmail.com>
2022-01-30 23:29:17 +0900

    Feat: user flow Enhance

--- darkChoco/views.py
+++ darkChoco/views.py
@@ -3,29 +3,22 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.contrib import messages
 from .models import Letter
+from whiteChoco.models import Profile
 
 def index(request):
-    try :
-
-        letters = Letter.objects.filter(to_user=request.user).order_by('-send_date')
-        context = {
-            'letters' : letters
-        }
-        return render(request, 'index.html', context)
-    except :  # 에러 종류
-        return render(request, 'index.html')
+    return render(request, 'index.html')
 
 def post(request):
     try :
-        username = User.objects.get(username=request.GET['to_user'])
+        profileModel = Profile.objects.get(nickname=request.GET['to_user'])
         letter = Letter()
-        letter.to_user = username
+        letter.to_user = profileModel.user
         letter.from_user = request.user.get_username()
         letter.letter_text = request.GET['letter_text']
         letter.send_date = timezone.datetime.now()
         letter.save()
         messages.info(request, '전송완료!')
         return redirect('index')
-    except :  # 에러 종류
+    except :
         messages.info(request, '아이디 없음!')
         return redirect('index')
