39e1a63d1a5a8abdc3fdf727ccd826f94c9631cb
integerJI <jjs9536@gmail.com>
2022-02-01 23:30:11 +0900

    Feat: with react MIDDLEWARE add

--- sweetProject/settings.py
+++ sweetProject/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # React와 연결 하기 위한 CORS 추가 2022.02.01 jsji
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
